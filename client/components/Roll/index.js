d9c0cf05d993540861861972841c3eb9f0abaff9
iddar <iddar@dbug.mx>
2016-08-21 02:52:37 -0500

    set basic rules

--- client/components/Roll/index.js
+++ client/components/Roll/index.js
@@ -3,12 +3,6 @@ import React, { Component } from 'react'
 import _ from 'lodash'
 import style from './style.css'
 
-const catalog = [
-  'https://pbs.twimg.com/profile_images/603255697002790912/dSoMJigf.jpg',
-  'https://pbs.twimg.com/profile_images/676801385405538304/M8hgumGw.jpg',
-  'http://ichef.bbci.co.uk/news/624/cpsprodpb/4232/production/_90464961_55d-394fda8f2225.jpg'
-]
-
 const end = [500, 750]
 
 class MainSection extends Component {
@@ -18,14 +12,16 @@ class MainSection extends Component {
     this.aniamtion = setInterval(this.renderImages.bind(this), this.time)
     this.timer = setInterval(this.changeTimer.bind(this), 1500)
     this.state = {
-      position: 0
+      position: 0,
+      item: 0
     }
   }
 
   changeTimer () {
     clearInterval(this.aniamtion)
     this.time = this.time * 1.5
-    this.aniamtion = setInterval(this.renderImages.bind(this), this.time * 1.1)
+    let fix = 1.1 + (_.random(1, 5) / 100)
+    this.aniamtion = setInterval(this.renderImages.bind(this), this.time * fix)
 
     if (this.time > end[_.random(0, 1)]) {
       clearInterval(this.aniamtion)
@@ -43,6 +39,11 @@ class MainSection extends Component {
     })
   }
 
+  componentWillUnmount () {
+    clearInterval(this.aniamtion)
+    clearInterval(this.timer)
+  }
+
   // renderImages () {
   //   return _.map([1, 2, 3], (position) => {
   //     return <img key={position} src={`http://lorempixel.com/250/250/cats/?q=${position}`} />
@@ -51,10 +52,11 @@ class MainSection extends Component {
 
   render () {
     let { position, item } = this.state
+    let { catalog } = this.props
 
     return (
       <div className={style.imageBox}>
-        <img key={position} className='animated flipInX' src={catalog[item]} />
+        <img key={position} className='animated flipInX' src={catalog[item].img} />
       </div>
     )
   }
--- client/containers/App/index.js
+++ client/containers/App/index.js
@@ -1,5 +1,6 @@
 
 import React, { Component } from 'react'
+import _ from 'lodash'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 
@@ -8,29 +9,82 @@ import Roll from '../../components/Roll'
 import * as TodoActions from '../../actions/todos'
 import style from './style.css'
 
+const catalog = [
+  {
+    img: 'https://pbs.twimg.com/profile_images/603255697002790912/dSoMJigf.jpg',
+    text: 'uno'
+  },
+  {
+    img: 'https://pbs.twimg.com/profile_images/676801385405538304/M8hgumGw.jpg',
+    text: 'dos'
+  },
+  {
+    img: 'http://ichef.bbci.co.uk/news/624/cpsprodpb/4232/production/_90464961_55d-394fda8f2225.jpg',
+    text: 'tres'
+  }
+]
+
 class App extends Component {
 
   constructor (props, context) {
     super(props, context)
-    this.results = []
-  }
-
-  onFinish (value) {
-    this.results.push(value)
-    if (this.results.length === 3) {
-      let isWin = this.results.every(e => this.results[0] === e)
-      console.warn(isWin)
+    this.results = [null, null, null]
+    this.state = {
+      game: 0,
+      finish: false,
+      win: false
     }
   }
 
+  onFinish (position, value) {
+    this.results[position] = value
+    if (!this.results.some(e => e === null)) {
+      let isWin = this.results.every(e => this.results[0] === e)
+
+      this.setState({
+        finish: true,
+        win: isWin
+      })
+    }
+  }
+
+  newGame () {
+    this.results = [null, null, null]
+    var game
+
+    do {
+      game = _.random(1, 10)
+    } while (game === this.state.game)
+
+    this.setState({
+      game: game,
+      finish: false
+    })
+  }
+
+  getText () {
+    let text = _.reduce(this.results, (sum, item) => {
+      if (sum === '') return catalog[item].text
+      return `${sum} ${catalog[item].text}`
+    }, '')
+
+    return _.capitalize(text)
+  }
+
   render () {
+    let { game, finish } = this.state
+
     return (
-      <div className={style.content}>
+      <div className={style.content} key={game}>
         <div className={style.screen}>
-          <Roll onFinish={this.onFinish.bind(this)} />
-          <Roll onFinish={this.onFinish.bind(this)} />
-          <Roll onFinish={this.onFinish.bind(this)} />
+          <Roll onFinish={this.onFinish.bind(this, 0)} catalog={catalog} />
+          <Roll onFinish={this.onFinish.bind(this, 1)} catalog={catalog} />
+          <Roll onFinish={this.onFinish.bind(this, 2)} catalog={catalog} />
         </div>
+
+        <div className={style.result}>{finish && this.getText()}</div>
+
+        <a onClick={finish ? this.newGame.bind(this) : null}>New game</a>
       </div>
     )
   }
